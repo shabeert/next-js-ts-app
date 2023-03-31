e9f604c7b199b06f417b38caf684ebd9d1ed22ce
Shabeer Thalapurath <Shabeer.Thalapurath-v@adityabirlacapital.com>
2023-03-31 13:59:39 +0530

    Adding CSS and JS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,15 @@
 import '@/styles/globals.css'
+import '@/styles/open-iconic-bootstrap.min.css'
+import '@/styles/animate.css'
+import '@/styles/owl.carousel.min.css'
+import '@/styles/owl.theme.default.min.css'
+import '@/styles/magnific-popup.css'
+import '@/styles/aos.css'
+import '@/styles/ionicons.min.css'
+import '@/styles/flaticon.css'
+import '@/styles/icomoon.css'
+import '@/styles/style.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Fredericka+the+Great"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
