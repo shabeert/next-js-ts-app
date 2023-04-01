4c4ee8c89bf9907de01e8e618d959572d461484b
09Tushar <114922918+09Tushar@users.noreply.github.com>
2023-04-01 14:40:08 +0530

    Banner Component with Dynamic Content

--- Components/banner.tsx
+++ Components/banner.tsx
@@ -1,49 +1,54 @@
 import React from "react";
+import { useState } from "react";
+
 function Banner() {
+  
+  const DUMMY_BANNERITEMS = [
+    {
+      id: "b1",
+      title: "Kids Are The Best Explorers In The World",
+      image: "images/bg_1.jpg",
+      link: "/",
+    },
+    {
+      id: "b2",
+      title: "Perfect Learned For Your Child",
+      image: "images/bg_2.jpg",
+      link: "/",
+    },
+  ];
+
+  const [bannerItems, setBannerItems] = useState(DUMMY_BANNERITEMS);
+
   return (
     <>
       <section className="home-slider owl-carousel">
-        <div className="slider-item" style={{backgroundImage:`url(images/bg_1.jpg)`}}>
-          <div className="overlay"></div>
-          <div className="container">
-            <div
-              className="row no-gutters slider-text align-items-center justify-content-center"
-              data-scrollax-parent="true"
-            >
-              <div className="col-md-8 text-center ftco-animate">
-                <h1 className="mb-4">
-                  Kids Are The Best <span>Explorers In The World</span>
-                </h1>
-                <p>
-                  <a href="#" className="btn btn-secondary px-4 py-3 mt-3">
-                    Read More
-                  </a>
-                </p>
+        {bannerItems.map((item) => (
+          <div
+            className="slider-item"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <div className="overlay"></div>
+            <div className="container">
+              <div
+                className="row no-gutters slider-text align-items-center justify-content-center"
+                data-scrollax-parent="true"
+              >
+                <div className="col-md-8 text-center ftco-animate">
+                  <h1 className="mb-4">{item.title}</h1>
+                  <p>
+                    <a
+                      href={item.link}
+                      className="btn btn-secondary px-4 py-3 mt-3"
+                    >
+                      Read More
+                    </a>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="slider-item" style={{backgroundImage:`url(images/bg_2.jpg)`}}>
-          <div className="overlay"></div>
-          <div className="container">
-            <div
-              className="row no-gutters slider-text align-items-center justify-content-center"
-              data-scrollax-parent="true"
-            >
-              <div className="col-md-8 text-center ftco-animate">
-                <h1 className="mb-4">
-                  Perfect Learned<span> For Your Child</span>
-                </h1>
-                <p>
-                  <a href="#" className="btn btn-secondary px-4 py-3 mt-3">
-                    Read More
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </section>
     </>
   );
