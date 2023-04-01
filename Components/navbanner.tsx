8245e630e844f338a9a6d0f16e1e6d348c36b7c4
Kartikey371 <kartikghatge07@gmail.com>
2023-04-01 20:21:57 +0530

    Navbanner Component

--- Components/navbanner.tsx
+++ Components/navbanner.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import { idText } from "typescript";
 
-function Banner(props: any) {
+
+function NavBanner(props: any) {
 
     const DUMMY_BANNERITEMS = [
         {
@@ -16,27 +16,30 @@ function Banner(props: any) {
     const [bannerItems, setBannerItems] = useState(props.items);
 
     return (
-        <>
-            <section class="hero-wrap hero-wrap-2">
-                {bannerItems.map((item: any) => (
-                    <div key={item.id}
-                        className="slider-item"
-                        style={{ backgroundImage: `url(${item.image})` }}
-                    >
-                        <div className="overlay"></div>
-                        <div className="container">
-                            <div class="row no-gutters slider-text align-items-center justify-content-center">
-                                <div class="col-md-9 ftco-animate text-center">
-                                    <h1 class="mb-2 bread">Our Courses</h1>
-                                    <p class="breadcrumbs"><span class="mr-2"><a href="#">Home
-                                        <i class="ion-ios-arrow-forward"></i></a></span> <span>Courses <i class="ion-ios-arrow-forward"></i></span></p>
-                                </div>
-                            </div>
+       
+            <>
+            
+                  <section class="hero-wrap hero-wrap-2">
+ 
+            <div key={bannerItems[0].id}
+                className="slider-item"
+                style={{ backgroundImage: `url(${bannerItems[0].image})` }}
+            >
+                <div className="overlay"></div>
+                <div className="container">
+                    <div class="row no-gutters slider-text align-items-center justify-content-center">
+                        <div class="col-md-9 ftco-animate text-center">
+                            <h1 class="mb-2 bread">Our Courses</h1>
+                            <p class="breadcrumbs"><span class="mr-2"><a href="#">Home
+                                <i class="ion-ios-arrow-forward"></i></a></span> <span>Courses <i class="ion-ios-arrow-forward"></i></span></p>
                         </div>
                     </div>
-                ))}
-            </section>
-        </>
+                </div>
+            </div>
+       
+    </section>
+       </>
+ 
     );
 }
-export default Banner;
+export default NavBanner;
--- pages/courses.tsx
+++ pages/courses.tsx
@@ -1,9 +1,11 @@
 import CoursesList from "../components/courses/courseslist"
 
 import axios from "axios";
+import NavBanner from "@/components/navbanner";
 function Courses(props:any) {
   return (
     <>
+    <NavBanner items = {props.navbanner} ></NavBanner>
      <CoursesList courses = {props.courses}></CoursesList>
     </>
   );
@@ -13,10 +15,14 @@ export const getStaticProps = async () => {
     `https://horizontal-demo-default-rtdb.firebaseio.com/courses.json `
   );
   const ourCoursesData= response.data;
-
+  response = await axios.get(
+    `https://horizontal-demo-default-rtdb.firebaseio.com/banners.json`
+  );
+  const navbannerData= response.data;
   return {
     props: {     
       courses: ourCoursesData,
+      navbanner : navbannerData
     },
   };
 };
