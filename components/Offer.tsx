aa4c57cdeeb7fbfce7002d4739b66dd80ef88b27
Dhruvi Patel <dhruvi.patel@horizontal.com>
2023-04-01 11:59:31 +0530

    added components

---
+++ components/Offer.tsx
@@ -0,0 +1,82 @@
+const Offer = () => {
+    return(
+        <>
+           <h1>Offer component</h1>
+           <section className="ftco-section ftco-no-pt ftc-no-pb">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-5 order-md-last wrap-about py-5 wrap-about bg-light">
+						<div className="text px-4 ftco-animate">
+	          	<h2 className="mb-4">Welcome to Kiddos Learning School</h2>
+							<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. And if she hasn’t been rewritten, then they are still using her.</p>
+							<p><a href="#" className="btn btn-secondary px-4 py-3">Read More</a></p>
+						</div>
+					</div>
+					<div className="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
+          	<h2 className="mb-4">What We Offer</h2>
+						<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word.</p>
+						<div className="row mt-5">
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-security"></span></div>
+									<div className="text">
+										<h3>Safety First</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-reading"></span></div>
+									<div className="text">
+										<h3>Regular Classes</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-diploma"></span></div>
+									<div className="text">
+										<h3>Certified Teachers</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-education"></span></div>
+									<div className="text">
+										<h3>Sufficient Classrooms</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-jigsaw"></span></div>
+									<div className="text">
+										<h3>Creative Lessons</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-6">
+								<div className="services-2 d-flex">
+									<div className="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span className="flaticon-kids"></span></div>
+									<div className="text">
+										<h3>Sports Facilities</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+        </>
+    )
+}
+export default Offer;
