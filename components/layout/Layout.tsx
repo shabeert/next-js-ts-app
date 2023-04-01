5deb942c02eb1a0735c337a0ade539594b4f2e1c
Garima Thakore <gthakore@horizontal.com>
2023-04-01 11:57:12 +0530

    layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,12 +5,12 @@ export default function Layout({ children }: {
     children: React.ReactNode;
   }) {
     return (
-      <html lang="en">
-        <body>
-            Test Content
-            {children}
-        </body>
-        <FooterComponent></FooterComponent>
-      </html>
+      
+          <><main>
+        {children}
+
+      </main><FooterComponent></FooterComponent></>
+            
+       
     );
   }
