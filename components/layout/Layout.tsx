5aa78423bae91439f38b621f0c826dcfed1fb10c
Garima Thakore <gthakore@horizontal.com>
2023-04-01 11:19:10 +0530

    commit

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,3 +1,6 @@
+import FooterComponent from "../footer";
+
+
 export default function Layout({ children }: {
     children: React.ReactNode;
   }) {
@@ -7,6 +10,7 @@ export default function Layout({ children }: {
             Test Content
             {children}
         </body>
+        <FooterComponent></FooterComponent>
       </html>
     );
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import FooterComponent from '@/Components/footer'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +19,7 @@ export default function Home() {
       <main className={styles.main}>
       <div>Main component</div>
       </main>
-      <FooterComponent></FooterComponent>
+      
     </>
   )
 }
