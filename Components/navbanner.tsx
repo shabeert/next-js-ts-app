b58870b2d574fac3ea1c499164469d59d0828162
Kartikey371 <kartikghatge07@gmail.com>
2023-04-02 10:00:48 +0530

    ClassName changes

--- Components/navbanner.tsx
+++ Components/navbanner.tsx
@@ -16,30 +16,30 @@ function NavBanner(props: any) {
     const [bannerItems, setBannerItems] = useState(props.items);
 
     return (
-       
-            <>
-            
-                  <section class="hero-wrap hero-wrap-2">
- 
-            <div key={bannerItems[0].id}
-                className="slider-item"
-                style={{ backgroundImage: `url(${bannerItems[0].image})` }}
-            >
-                <div className="overlay"></div>
-                <div className="container">
-                    <div class="row no-gutters slider-text align-items-center justify-content-center">
-                        <div class="col-md-9 ftco-animate text-center">
-                            <h1 class="mb-2 bread">Our Courses</h1>
-                            <p class="breadcrumbs"><span class="mr-2"><a href="#">Home
-                                <i class="ion-ios-arrow-forward"></i></a></span> <span>Courses <i class="ion-ios-arrow-forward"></i></span></p>
+
+        <>
+
+            <section className="hero-wrap hero-wrap-2">
+
+                <div key={bannerItems[0].id}
+                    className="slider-item"
+                    style={{ backgroundImage: `url(${bannerItems[0].image})` }}
+                >
+                    <div className="overlay"></div>
+                    <div className="container">
+                        <div className="row no-gutters slider-text align-items-center justify-content-center">
+                            <div className="col-md-9 ftco-animate text-center">
+                                <h1 className="mb-2 bread">Our Courses</h1>
+                                <p className="breadcrumbs"><span className="mr-2"><a href="/">Home
+                                    <i className="ion-ios-arrow-forward"></i></a></span> <span>Courses <i className="ion-ios-arrow-forward"></i></span></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-       
-    </section>
-       </>
- 
+
+            </section>
+        </>
+
     );
 }
 export default NavBanner;
