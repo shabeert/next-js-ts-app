33892af4df942ea1abc7bdbd7f3d65ed35dd6e08
Dhruvi Patel <dhruvi.patel@horizontal.com>
2023-04-02 13:09:35 +0530

    contact us card component

--- Components/contactUsCards.tsx
+++ Components/contactUsCards.tsx
@@ -1,37 +1,42 @@
-const ContactUsCards = () => {
-    return(
-        <>
-           <section className="ftco-section contact-section">
-      <div className="container">
-        <div className="row d-flex mb-5 contact-info">
-          <div className="col-md-3 d-flex">
-          	<div className="bg-light align-self-stretch box p-4 text-center">
-          		<h3 className="mb-4">Address</h3>
-	            <p>198 West 21th Street, Suite 721 New York NY 10016</p>
-	          </div>
+import Link from "next/link";
+type CardsProps = {
+  cards: Cards[];
+};
+
+interface Cards {
+  id: string;
+  title: string;
+  detail: string;
+  link: string;
+}
+
+
+const ContactUsCards = (props: CardsProps) => {
+  const cardData = props.cards;
+  return (
+    <>
+      <section className="ftco-section contact-section">
+        <div className="container">
+          <div className="row d-flex mb-5 contact-info">
+          {
+            cardData.map((card) => 
+            <div key={card.id} className="col-md-3 d-flex">
+            <div className="bg-light align-self-stretch box p-4 text-center">
+              <h3 className="mb-4">{card.title}</h3>
+              {
+                card.link === "" ? <p>{card.detail}</p> : <p><Link href={card.link}>{card.detail}</Link></p>
+              }
+
+            </div>
           </div>
-          <div className="col-md-3 d-flex">
-          	<div className="bg-light align-self-stretch box p-4 text-center">
-          		<h3 className="mb-4">Contact Number</h3>
-	            <p><a href="tel://1234567920">+ 1235 2355 98</a></p>
-	          </div>
-          </div>
-          <div className="col-md-3 d-flex">
-          	<div className="bg-light align-self-stretch box p-4 text-center">
-          		<h3 className="mb-4">Email Address</h3>
-	            <p><a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	          </div>
-          </div>
-          <div className="col-md-3 d-flex">
-          	<div className="bg-light align-self-stretch box p-4 text-center">
-          		<h3 className="mb-4">Website</h3>
-	            <p><a href="#">yoursite.com</a></p>
-	          </div>
+            )
+          }
+            
+
           </div>
         </div>
-      </div>
-    </section>
-        </>
-    )
+      </section>
+    </>
+  )
 }
 export default ContactUsCards;
--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -6,6 +6,7 @@ import Layout from '@/components/layout/Layout'
 import TeachersList from '@/components/teachers/TeachersList'
 import ContactUsForm from '@/components/forms/contactus'
 import axios from 'axios'
+import ContactUsCards from '@/components/contactUsCards'
 
 
 
@@ -14,7 +15,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Teachers(props : any) {
   return (
     <>     
-     
+     <ContactUsCards cards={props.contactUsCards}/>
       <TeachersList teachers={props.teachers}></TeachersList>
       <ContactUsForm></ContactUsForm>
       
@@ -29,9 +30,15 @@ export async function getServerSideProps() {
     `https://horizontal-demo-default-rtdb.firebaseio.com/teachers.json`
   );
   const teachersData = response.data;
+
+  var response = await axios.get(
+    `https://horizontal-demo-default-rtdb.firebaseio.com/contactuscards.json`
+  );
+  const contactUsCardsData = response.data;
   return {
     props: {
       teachers: teachersData,
+      contactUsCards: contactUsCardsData,
     },
   };
 }
